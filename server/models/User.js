1e1553f870f828fdcfa93b2ea0082b12ba7eaef9
Alison Buitrago <alisonbuitrago217@gmail.com>
2025-07-10 15:53:00 +0200

    Cambios en user.js

--- server/models/User.js
+++ server/models/User.js
@@ -1,14 +1,31 @@
-// server/models/User.js
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
 
 const UserSchema = new mongoose.Schema({
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true }
+  googleId: {
+    type: String,
+    default: null, // Para usuarios que se registran con Google
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: function () {
+      return !this.googleId; // Solo requerido si no es OAuth
+    },
+  },
+  username: {
+    type: String,
+    default: null, 
+  }
 });
 
+// Encriptar contraseña si el usuario no es OAuth
 UserSchema.pre("save", async function (next) {
-  if (!this.isModified("password")) return next();
+  if (!this.isModified("password") || this.googleId) return next();
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
   next();
